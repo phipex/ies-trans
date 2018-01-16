4080b529ee8b3aa406cb9f7e9031e50099ceb5ab
desarrollo-pc <aphipex@gmail.com>
2018-01-16 10:43:48 -0500

    feature(transaction): campo para los bonus restricted

--- src/main/java/co/com/ies/service/dto/PlayRequestDTO.java
+++ src/main/java/co/com/ies/service/dto/PlayRequestDTO.java
@@ -5,7 +5,7 @@ import java.time.ZonedDateTime;
 import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Objects;
+
 import co.com.ies.domain.enumeration.PlayType;
 
 /**
@@ -36,6 +36,8 @@ public class PlayRequestDTO implements Serializable {
     private BigDecimal balance;
 
     private BigDecimal bonusbalance;
+    
+    private BigDecimal bonusRestrictedBalance;
 
     @NotNull
     private String user;
@@ -139,42 +141,117 @@ public class PlayRequestDTO implements Serializable {
         this.operator = operator;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+	public BigDecimal getBonusRestrictedBalance() {
+		return bonusRestrictedBalance;
+	}
 
-        PlayRequestDTO playRequestDTO = (PlayRequestDTO) o;
-        if(playRequestDTO.getId() == null || getId() == null) {
-            return false;
-        }
-        return Objects.equals(getId(), playRequestDTO.getId());
-    }
+	public void setBonusRestrictedBalance(BigDecimal bonusRestrictedBalance) {
+		this.bonusRestrictedBalance = bonusRestrictedBalance;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
+	@Override
+	public String toString() {
+		return "PlayRequestDTO [id=" + id + ", playtype=" + playtype + ", token=" + token + ", system=" + system
+				+ ", timestamp=" + timestamp + ", ticketid=" + ticketid + ", transactionid=" + transactionid
+				+ ", amount=" + amount + ", balance=" + balance + ", bonusbalance=" + bonusbalance
+				+ ", bonusRestrictedBalance=" + bonusRestrictedBalance + ", user=" + user + ", operator=" + operator
+				+ "]";
+	}
 
-    @Override
-    public String toString() {
-        return "PlayRequestDTO{" +
-            "id=" + getId() +
-            ", playtype='" + getPlaytype() + "'" +
-            ", token='" + getToken() + "'" +
-            ", system='" + getSystem() + "'" +
-            ", timestamp='" + getTimestamp() + "'" +
-            ", ticketid='" + getTicketid() + "'" +
-            ", transactionid='" + getTransactionid() + "'" +
-            ", amount=" + getAmount() +
-            ", balance=" + getBalance() +
-            ", bonusbalance=" + getBonusbalance() +
-            ", user='" + getUser() + "'" +
-            ", operator='" + getOperator() + "'" +
-            "}";
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result + ((balance == null) ? 0 : balance.hashCode());
+		result = prime * result + ((bonusRestrictedBalance == null) ? 0 : bonusRestrictedBalance.hashCode());
+		result = prime * result + ((bonusbalance == null) ? 0 : bonusbalance.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((operator == null) ? 0 : operator.hashCode());
+		result = prime * result + ((playtype == null) ? 0 : playtype.hashCode());
+		result = prime * result + ((system == null) ? 0 : system.hashCode());
+		result = prime * result + ((ticketid == null) ? 0 : ticketid.hashCode());
+		result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		result = prime * result + ((transactionid == null) ? 0 : transactionid.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayRequestDTO other = (PlayRequestDTO) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (balance == null) {
+			if (other.balance != null)
+				return false;
+		} else if (!balance.equals(other.balance))
+			return false;
+		if (bonusRestrictedBalance == null) {
+			if (other.bonusRestrictedBalance != null)
+				return false;
+		} else if (!bonusRestrictedBalance.equals(other.bonusRestrictedBalance))
+			return false;
+		if (bonusbalance == null) {
+			if (other.bonusbalance != null)
+				return false;
+		} else if (!bonusbalance.equals(other.bonusbalance))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (operator == null) {
+			if (other.operator != null)
+				return false;
+		} else if (!operator.equals(other.operator))
+			return false;
+		if (playtype != other.playtype)
+			return false;
+		if (system == null) {
+			if (other.system != null)
+				return false;
+		} else if (!system.equals(other.system))
+			return false;
+		if (ticketid == null) {
+			if (other.ticketid != null)
+				return false;
+		} else if (!ticketid.equals(other.ticketid))
+			return false;
+		if (timestamp == null) {
+			if (other.timestamp != null)
+				return false;
+		} else if (!timestamp.equals(other.timestamp))
+			return false;
+		if (token == null) {
+			if (other.token != null)
+				return false;
+		} else if (!token.equals(other.token))
+			return false;
+		if (transactionid == null) {
+			if (other.transactionid != null)
+				return false;
+		} else if (!transactionid.equals(other.transactionid))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+
+    
+    
 }
